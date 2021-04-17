f3d96dfd2a86e18336ff4f15b4bb5cd15964503a
Diego Zuniga <devdizu@gmail.com>
2021-04-17 18:41:45 -0500

    :sparkles: Redirecting to home when user press click on logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import Search from "../search";
 
 interface Props {
@@ -11,14 +12,18 @@ export default class Header extends React.Component<Props> {
   render() {
     return (
       <div className={"bg-yellow " + (this.props.className ?? "")}>
-        <div className={"container mx-auto max-w-screen-lg px-3 h-12 flex items-center"}>
-          <Image
-            className="flex-shrink-0"
-            src="/images/ml-logo.svg"
-            alt="Mercadolibre"
-            width={34}
-            height={34}
-          />
+        <div className="container mx-auto max-w-screen-lg px-3 h-12 flex items-center">
+          <Link href="/">
+            <a>
+              <Image
+                className="flex-shrink-0"
+                src="/images/ml-logo.svg"
+                alt="Mercadolibre"
+                width={34}
+                height={34}
+              />
+            </a>
+          </Link>
           <Search
             className="ml-3 flex-grow"
             searchQuery={this.props.searchQuery}
